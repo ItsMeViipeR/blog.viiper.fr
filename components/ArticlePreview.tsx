c209b080c3c0b539fc658666ca0096f0066f44b1
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-12-04 20:37:15 +0100

    feat: :sparkles: Add article preview component

---
+++ components/ArticlePreview.tsx
@@ -0,0 +1,27 @@
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface ArticlePreviewProps {
+  title: string;
+  description: string;
+  img: Image;
+}
+
+export const ArticlePreview = (props: ArticlePreviewProps) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={props.img.src} alt={props.img.alt} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{props.title}</h2>
+        <p>{props.description}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Read</button>
+        </div>
+      </div>
+    </div>
+  );
+};
