f2f0ae703935450f5a6b3a712c19063393e1e293
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-12-06 16:36:59 +0100

    refactor: :recycle: Remove "You have to be admin"

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -88,18 +88,18 @@ export function Navbar() {
               </a>
             </li>
 
-            <li>
-              {session.data && isAdmin ? (
+            {session && isAdmin ? (
+              <li>
                 <a
                   href="/admin"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-colors duration-100"
                 >
                   Admin
                 </a>
-              ) : (
-                <>You have to be admin</>
-              )}
-            </li>
+              </li>
+            ) : (
+              <></>
+            )}
             <li>{session.data ? <LogoutButton /> : <LoginButton />}</li>
           </ul>
         </div>
