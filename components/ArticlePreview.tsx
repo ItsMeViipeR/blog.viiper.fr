2328e63ee5c6ce70ebfe18d3be426a5f579248af
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-12-08 11:05:16 +0100

    style: :lipstick: Add margin for read button

--- components/ArticlePreview.tsx
+++ components/ArticlePreview.tsx
@@ -14,13 +14,17 @@ export const ArticlePreview = (props: ArticlePreviewProps) => {
     <div className="card w-96 bg-base-300 shadow-xl">
       <figure className="h-64">
         {props.img !== undefined && (
-          <img src={props.img?.src} alt={props.img?.alt} />
+          <img
+            src={props.img?.src}
+            alt={props.img?.alt}
+            className="object-cover aspect-video h-full"
+          />
         )}
       </figure>
       <div className="card-body">
         <h2 className="card-title">{props.title}</h2>
         <p>{props.description}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions justify-end mt-2">
           <button className="btn btn-primary">Read</button>
         </div>
       </div>
