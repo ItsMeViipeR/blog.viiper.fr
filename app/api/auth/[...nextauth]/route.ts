bc1f5ad8f757d911ec4facad281b3241dcee1144
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-12-08 10:43:13 +0100

    fix: :bug: Fix database writing error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 const googleId = process.env.GOOGLE_ID!;
 const googleSecret = process.env.GOOGLE_SECRET!;
+const nextAuthSecret = process.env.NEXTAUTH_SECRET!;
 
 export const authOptions = {
   providers: [
@@ -14,6 +15,9 @@ export const authOptions = {
     }),
   ],
   adapter: PrismaAdapter(prisma),
+  secret: nextAuthSecret,
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
